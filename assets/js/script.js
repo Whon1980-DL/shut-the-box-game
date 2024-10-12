d7020e2cb9911ee81de9897f3bc2655c6fcb591f
Supitpong Wongsaroj <ewrctysf@students.codeinstitute.net>
2024-10-12 16:11:21 +0000

    Add code to display and hide rules box modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-console.log("Hello");
+//Function for setting game rules box as modal box
+let modal = document.getElementById("game-rules");
+let btn = document.getElementById("rules-btn");
+let close = document.getElementsByClassName("close")[0];
+
+function showRules() {
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+}

4582c43d654528c667e86886b0f2d7677dfb6651
Supitpong Wongsaroj <ewrctysf@students.codeinstitute.net>
2024-10-20 17:14:06 +0000

    Update fliptile function

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,7 +87,7 @@ function flipTile(event) {
         chosenTileValue = 0;
         console.log('sumOfDice == 0');
         return;
-    } else if (chosenTileValue > sumOfDice && tempFlippedTileArray.length === 0 && flippedTileArray.length == 8) {  
+    } else if (chosenTileValue > sumOfDice && tempFlippedTileArray.length === 0 && flippedTileArray.length >= 7) {  
         alertMsg.style.display = 'block';
         alertMsg.innerHTML = "Game Over!";
         calculateScore();
@@ -103,7 +103,7 @@ function flipTile(event) {
         console.log('chosenTileValue > sumOfDice');
         console.log("sumOfDice == 0");
         return;
-    } else if (chosenTileValue < sumOfDice && tempFlippedTileArray.length === 0 && flippedTileArray == 8) {
+    } else if (chosenTileValue < sumOfDice && tempFlippedTileArray.length === 0 && flippedTileArray >= 7) {
         pickedTileOne = tempFlippedTileArray[0];
         console.log(pickedTileOne);
         document.getElementById('tile' + event).style.backgroundColor = "brown";
